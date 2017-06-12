79975b4c53dc2c226a73f39d472584c739674906
Billy.Zheng <zw963@163.com>
2017-06-12 14:39:25 +0800

    使用最简单的办法让 http_server 工作.

--- src/http.rb
+++ src/http.rb
@@ -1,5 +1,11 @@
 require 'nodejs'
 
 http = node_require('http')
+port = 1337
 
-$console.log http
+`
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
+`
