477a9bbde0943ce534e941306f39d236f8471afb
Billy.Zheng <zw963@163.com>
2017-06-12 15:14:30 +0800

    refactor

--- src/http.rb
+++ src/http.rb
@@ -3,7 +3,15 @@ require 'nodejs'
 http = node_require('http')
 port = 1337
 
-http.JS.createServer(lambda {|req, res|
-    res.JS.writeHead(200, { 'Content-Type': 'text/plain' }.to_n)
+# `
+# http.createServer(function(req, res) {
+#   res.writeHead(200, { 'Content-Type': 'text/plain' });
+#   res.end('Hello World\n');
+# }).listen(port);
+# `
+
+# 这是上面 js 代码的等价代码
+http.JS.createServer(->(_req, res) {
+    res.JS.writeHead(200, {'Content-Type': 'text/plain'}.to_n)
     res.JS.end("Hello World\n")
   }).JS.listen(port)
