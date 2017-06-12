eaa5ee690e0eb47e21607a0830d5fdde989145fc
Billy.Zheng <zw963@163.com>
2017-06-12 15:48:09 +0800

    改为假想的更加 ruby-fy 的工作方式.

--- src/http.rb
+++ src/http.rb
@@ -19,10 +19,18 @@ port = 1337
 
 # 上面的调用, 使用了四次 .JS 语法, 这看起来不太舒服, 因此使用 method_missing
 
-http_wrapper = Native(http)
+# http_wrapper = Native(http)
 
-http_wrapper.createServer(->(_req, res) {
-    opal_res = Native(res)
-    opal_res.writeHead(200, {'Content-Type': 'text/plain'})
-    opal_res.end("Hello World\n")
-  }).listen(port)
+# http_wrapper.createServer(->(_req, res) {
+#     opal_res = Native(res)
+#     opal_res.writeHead(200, {'Content-Type': 'text/plain'})
+#     opal_res.end("Hello World\n")
+#   }).listen(port)
+
+# 但是以上方式仍然不够好, 因为我们仍旧需要调用 wrap Native 两次, 而且也不是纯 Ruby 代码.
+# 我们期望的是下面这种更 Ruby 的解决方案. (这个 commit 并不工作.)
+
+HTTP::Server.listen(port) do |req, res|
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end "Hello World\n"
+end
